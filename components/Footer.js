bbbad534419471363bb3d53af5d1e3c70dda28d5
Carl Aragoncillo <carljasoncainaragoncillo@gmail.com>
2023-07-04 21:22:43 +0800

    removed some icons, added email

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
 import { Flex } from '@mantine/core';
-import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
-import { SiUpwork } from "react-icons/si";
+import { SiGmail } from "react-icons/si";
 
 const Footer = ({classes}) => {
     const date = new Date();
+    const handleClick = () => {
+      navigator.clipboard.writeText('carljasoncainaragoncillo@gmail.com');
+      window.alert('Email copied to clipboard');
+    };
     return (
       <Flex   
         justify="space-between"
@@ -28,28 +32,6 @@ const Footer = ({classes}) => {
 
         <li className={classes.icons}>
             <a
-            href="https://www.upwork.com/freelancers/~012c65944a4dfd29e6"
-            style={{ color: "black" }}
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <SiUpwork />
-            </a>
-        </li>
-
-        <li className={classes.icons}>
-            <a
-            href="https://twitter.com/krochet94"
-            className="text-white"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <AiOutlineTwitter />
-            </a>
-        </li>
-
-        <li className={classes.icons}>
-            <a
             href="https://github.com/krochet94"
             className="text-white"
             target="_blank"
@@ -58,6 +40,11 @@ const Footer = ({classes}) => {
             <AiFillGithub />
             </a>
         </li>
+
+        <li className={classes.icons} onClick={handleClick}>
+            <SiGmail />
+        </li>
+
         </ul>
         <h5>Copyright © {date.getFullYear()} krochet</h5>
       </Flex>
